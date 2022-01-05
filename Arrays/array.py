01d10541e9947e7a2f80c19ccba4059305b6c289
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2022-01-05 12:00:39 +0500

    Delete array.py

--- Arrays/array.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-    seen = []
-    for place, val in enumerate(nums):
-	req = target - val
-        if req in seen:
-            return [seen.index(req), place]
-        else:
-            seen.append(val)
-    print('not found any')
